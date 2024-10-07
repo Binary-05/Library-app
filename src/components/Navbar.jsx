eb5b9d59891e6ab86b3e3a34e48f0359d45539aa
Henrrike Quarshie <henrrikequarshie@gmail.com>
2024-10-07 10:42:19 +0000

    first-project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (
+        <div>
+           <nav>
+            <h1>Bookle</h1>
+            <a href="#">Home</a>
+            <a href="#">Books</a>
+            <a href="#">Add Book</a>
+            <a href="#">Pages</a>
+            <a href="#">Contact</a>
+           </nav>
+        </div>
+    )
+};
+
+export default Navbar;
